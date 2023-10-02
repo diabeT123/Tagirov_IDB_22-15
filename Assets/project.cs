a9662de88ccd204415162c5aa21cd01b845d41e0
diabeT123 <sdtagirov2005@gmail.com>
2023-10-02 22:54:01 +0300

    first

---
+++ Assets/project.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class project : MonoBehaviour
+{
+    public GameObject sphere;
+    public GameObject tree;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+    public void start_sphere ()
+    {
+     sphere.SetActive(!sphere.activeSelf);
+    }
+    public void start_tree()
+    {
+     tree.SetActive(!tree.activeSelf);
+    }
+}
